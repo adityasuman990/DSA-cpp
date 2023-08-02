58c20f761b519c59a5c758b7352b17bd2ef1d7f5
adityasuman <adityasuman@gmail.com>
2023-08-02 17:34:10 +0530

    2d to oned

---
+++ 2dArray/columnconversion.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int row,column;
+    // int i,;
+    cout<<"enter the no of rows:"<<endl;
+    cin>>row;
+    cout<<"enter the number of column:"<<endl;
+    cin>>column;
+    int arr[row][column];
+    cout<<"enter the elements"<<endl;
+     for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"the elements are:"<<endl;
+ for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<"Your multiDimentional array in one-d Array"<<endl;
+int n=row*column;
+int arrone[n];
+int r=0;
+
+
+ for (int j = 0; j < column; j++) {
+        for (int i = 0; i < row; i++) {
+            arrone[r]=arr[i][j];
+            r++;
+        }
+        cout<<endl;
+    }
+
+    cout<<"in 2d array"<<endl;
+    for(int q=0;q<n;q++){
+        cout<<arrone[q]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
