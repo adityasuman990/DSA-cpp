1b12356559235833375e6c0db214ba227c5da196
adityasuman <adityasuman990@gmail.com>
2023-08-04 17:28:26 +0530

    no third variable swap

---
+++ programs/nothirdvar.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    a=5;
+    b=6;
+    cout<<"a:"<<a<<"\nb:"<<b<<endl;
+    a=a+b;     
+b=a-b;     
+a=a-b; 
+ cout<<"a:"<<a<<"\nb:"<<b<<endl;
+    return 0;
+}
